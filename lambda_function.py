39130e3f3b270c599d3fcc82d6c4d44507224eae
dsthiago <thiagosd.2.18@gmail.com>
2024-07-09 18:20:21 -0300

    chore: Update gitignore to include reqs.zip file and remove unused code

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,11 @@
-from app.main import app
+from flask import Flask
 import awsgi
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
 
 def handler(event, context):
     return awsgi.response(app, event, context)
