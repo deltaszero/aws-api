a2a89a873a6e2fc06faecaa9806af633c31e1d87
dsthiago <thiagosd.2.18@gmail.com>
2024-07-11 14:47:16 -0300

    ```text chore: Update boto3 dependency to version 1.34.143
    Update the boto3 dependency in the poetry.lock and pyproject.toml files to version 1.34.143. This update ensures compatibility with the latest version of the AWS SDK for Python and improves the functionality of the application.

--- app/main.py
+++ app/main.py
@@ -1,4 +1,26 @@
+import boto3
+import json
+from botocore.exceptions import ClientError
 from flask import Flask, jsonify, request, render_template
+
+
+def get_secret():
+    secret_name = "whatsapp_api_secrets"
+    region_name = "sa-east-1"
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as _error_:
+        raise _error_
+    secret = get_secret_value_response['SecretString']
+    return json.loads(secret)
+
 
 app = Flask(__name__)
 
